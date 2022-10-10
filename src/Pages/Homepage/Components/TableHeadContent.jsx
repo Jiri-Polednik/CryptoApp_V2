33a4456c103b9c97c6aa7f1f382ba1d94dc6f5ba
Jiří Poledník <106665722+Jiri-Polednik@users.noreply.github.com>
2022-10-10 23:53:03 +0200

    CryptoApp: Dividing CoinsTable.jsx to separate components

---
+++ src/Pages/Homepage/Components/TableHeadContent.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { TableCell, TableRow } from '@mui/material'
+
+function TableHeadContent() {
+  return (
+    <TableRow>
+      {['Coin', 'Price', '24h Change', 'Market Cap'].map((header) => (
+        <TableCell sx={{ fontWeight: '600' }} key={header} align={header === 'Coin' ? 'left' : 'right'}>
+          <p>{header}</p>
+        </TableCell>
+      ))}
+    </TableRow>
+  )
+}
+
+export default TableHeadContent
